380d5fc952f0c4004ce828306e3c313d2bc6b40b
Epinicion <omahagoehring@gmail.com>
2014-01-16 18:47:15 -0600

    fixed

--- config/commands.js
+++ config/commands.js
@@ -971,7 +971,7 @@ max: function(target, room, user) {if (!this.canBroadcast()) return; this.sendRe
                 '<b>Trainer:</b> Maxerus <br \>' +
 		'<b>Ace:</b> Gliscor <br \>' + 
 		'<br \>' +		
-'<img src="http://www.pkparaiso.com/imagenes/xy/sprites/animados/gliscor.gif">+ '<img src="http://www.pkparaiso.com/imagenes/xy/sprites/animados/zapdos.gif">') 
+'<img src="http://www.pkparaiso.com/imagenes/xy/sprites/animados/gliscor.gif">'+ '<img src="http://www.pkparaiso.com/imagenes/xy/sprites/animados/zapdos.gif">') 
 },
 epin: function(target, room, user) {if (!this.canBroadcast()) return; this.sendReplyBox(
                 '<b>Trainer:</b> Epinicion <br \>' +
