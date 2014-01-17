37ba069ae438f6b777963b9e4d9ed4a2387f5304
Epinicion <omahagoehring@gmail.com>
2014-01-16 18:40:29 -0600

    New Gif for max

--- config/commands.js
+++ config/commands.js
@@ -971,7 +971,7 @@ max: function(target, room, user) {if (!this.canBroadcast()) return; this.sendRe
                 '<b>Trainer:</b> Maxerus <br \>' +
 		'<b>Ace:</b> Gliscor <br \>' + 
 		'<br \>' +		
-'<img src="http://pldh.net/media/pokemon/gen5/blackwhite_animated_front/472.gif">') 
+'<img src="http://www.pkparaiso.com/imagenes/xy/sprites/animados/gliscor.gif">') 
 },
 epin: function(target, room, user) {if (!this.canBroadcast()) return; this.sendReplyBox(
                 '<b>Trainer:</b> Epinicion <br \>' +
