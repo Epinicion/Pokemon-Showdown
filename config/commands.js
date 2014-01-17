bf610faa9631a03c773e15163af9a810006e31d8
Epinicion <omahagoehring@gmail.com>
2014-01-16 18:21:10 -0600

    Added Tour commands

--- config/commands.js
+++ config/commands.js
@@ -949,6 +949,24 @@ var commands = exports.commands = {
 			return this.sendReplyBox('Pokemon, item, move, or ability not found for generation ' + generation.toUpperCase() + '.');
 		}
 	},
+
+  //TOUR COMMANDS
+            
+    tourcommands: function(target, room, user) {
+        if (!this.canBroadcast()) return;
+        this.sendReplyBox('Tournaments through /tour can be started by Voice (+) users and higher:<br \>' +
+        '/tour [tier], [size] - Starts a tournament<br \>' +
+                '/endtour - Ends a currently running tournament<br \>' +
+                '/fj [username] - Force someone to join a tournament<br \>' +
+                '/fl [username] - Force someone to leave a tournament<br \>' +
+                '/toursize [size] - Changes the size of a currently running tournament<br \>' +
+                '/replace [username], [username] - Replaces user in a tournament with the second user');
+    },
+
+        /***************************************
+        * Trainer Cards *
+        ***************************************/
+
 max: function(target, room, user) {if (!this.canBroadcast()) return; this.sendReplyBox(
                 '<b>Trainer:</b> Maxerus <br \>' +
 		'<b>Ace:</b> Gliscor <br \>' + 
