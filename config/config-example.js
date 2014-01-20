5e71975f99ab99870b942dfbb710201936558a8a
EpicMarcus2000 <j_mgman@hotmail.com>
2014-01-20 14:48:17 -0500

    Update config-example.js

--- config/config-example.js
+++ config/config-example.js
@@ -136,7 +136,7 @@ exports.herokuhack = false;
 // Your server *must* be registered in order for your custom avatars to be
 // displayed in the client.
 exports.customavatars = {
-	'proteanshakes':'proteanshakes.gif';
+	'proteanshakes':'ProteanShakes.gif';
 	//'userid': 'customavatar.png'
 };
 
