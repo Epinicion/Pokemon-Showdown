38e6b208d094e3d138c7244defa1fb9ec4d9d87b
Epinicion <omahagoehring@gmail.com>
2014-01-23 22:34:34 -0600

    try that

--- config/commands.js
+++ config/commands.js
@@ -1039,7 +1039,7 @@ enty: function(target, room, user) {if (!this.canBroadcast()) return; this.sendR
 '<img src="http://www.pkparaiso.com/imagenes/xy/sprites/animados/dragonite-2.gif">'+ '<img src="http://www.pkparaiso.com/imagenes/xy/sprites/animados/arcanine.gif">'+ '<img src="http://www.pkparaiso.com/imagenes/xy/sprites/animados/garchomp.gif">')
 },
 strider: function(target, room, user) {if (!this.canBroadcast()) return; this.sendReplyBox(
-                '<font color="#00faff"><font size="4"><b>Prof. Aspen</b></font><br\><br\>' +
+                '<font color="#c600d8"><font size="4"><b>Prof. Aspen</b></font></font><br\><br\>' +
 		'<b>What I can help you with!:</b><br\>' + 
                 '<b> Training:</b> I have trained numerous current and former members, namely former champions Gabe and Lights, and current champ Epin.<br\>' + '<b>Instruct and Assist:</b> I can assist you in challenging the league and applying for a gym leader spot.<br\>' + '<b>Direct:</b> I can help you contact the correct people and find necessary information for your use and pleasure<br\>' )
 },
