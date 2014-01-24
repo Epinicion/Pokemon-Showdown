e2a7226ddd35b768edf2450a75ef4ee8ec811587
Epinicion <omahagoehring@gmail.com>
2014-01-23 19:09:42 -0600

    epin fic

--- config/commands.js
+++ config/commands.js
@@ -1074,7 +1074,7 @@ kyo: function(target, room, user) {if (!this.canBroadcast()) return; this.sendRe
 		'Dont underestimate the drizzle. Its just the calm before the storm<br \>' +
 		'<img src="http://www.pkparaiso.com/imagenes/xy/sprites/animados/kyogre.gif">'+ '<img src="http://www.pkparaiso.com/imagenes/xy/sprites/animados/manaphy.gif">'+ '<img src="http://www.pkparaiso.com/imagenes/xy/sprites/animados/tentacruel.gif">')},
 epin: function(target, room, user) {if (!this.canBroadcast()) return; this.sendReplyBox(
-                ''<font color="#00cf0a"><font size="4"><b>Champion:</b></font> Epinicion<br\>'  +
+                '<font color="#00cf0a"><font size="4"><b>Champion:</b></font> Epinicion<br\>'  +
 		'<b>Ace:</b> Crustle <br \>' + 
 		'Children need encouragement. So if a kid gets an answer right, tell him it was a lucky guess. That way, he develops a good, lucky feeling. <br \>' +
 		'<img src="http://www.pkparaiso.com/imagenes/xy/sprites/animados/crustle.gif" alt="Crustle"/>'+ '<img src="http://www.pkparaiso.com/imagenes/xy/sprites/animados/bisharp.gif">' + '<img src="http://www.pkparaiso.com/imagenes/xy/sprites/animados/genesect.gif">' + '<img src="http://www.pkparaiso.com/imagenes/xy/sprites/animados/mew.gif">' + '<img src="http://www.pkparaiso.com/imagenes/xy/sprites/animados/rayquaza.gif">'+ '<img src="http://www.pkparaiso.com/imagenes/xy/sprites/animados/hitmonlee.gif">' + '<img src="http://www.pkparaiso.com/imagenes/xy/sprites/animados/jolteon.gif">' + '<img src="http://www.pkparaiso.com/imagenes/xy/sprites/animados/treecko.gif">')},
