7d10185c4f0d5a645e9acf53925b481fd1bf1ef4
Epinicion <omahagoehring@gmail.com>
2014-01-25 12:08:00 -0600

    icey

--- config/commands.js
+++ config/commands.js
@@ -1033,6 +1033,12 @@ vain: function(target, room, user) {if (!this.canBroadcast()) return; this.sendR
 		'I am HIGH PRIEST VATICAN ASSASIN WARLOCK!<br \>' +
 '<img src="http://www.pkparaiso.com/imagenes/xy/sprites/animados/zoroark.gif">'+ '<img src="http://www.pkparaiso.com/imagenes/xy/sprites/animados/umbreon.gif">'+ '<img src="http://www.pkparaiso.com/imagenes/xy/sprites/animados/sableye.gif">')
 },
+icicle: function(target, room, user) {if (!this.canBroadcast()) return; this.sendReplyBox(
+                '<b>Trainer:</b> Icicle <br \>' +
+		'<b>Ace:</b> Abomasnow <br \>' +
+		'Icy alot of puns don't you?<br \>' +
+'<img src="http://www.pkparaiso.com/imagenes/xy/sprites/animados/mamoswine.gif">'+ '<img src="http://www.pkparaiso.com/imagenes/xy/sprites/animados/abomasnow.gif">'+ '<img src="http://www.pkparaiso.com/imagenes/xy/sprites/animados/cloyster.gif">')
+},
 radar: function(target, room, user) {if (!this.canBroadcast()) return; this.sendReplyBox(
                 '<b>Trainer:</b> Radar <br \>' +
 		'Never gave Epin a quote so she is a butthead that is related to Max, ew.<br \>' +
